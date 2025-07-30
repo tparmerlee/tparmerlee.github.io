cbcc1c5b0d447b4a91379a98308c7c2b16318213
Tristan Parmerlee <parmerleetris@gmail.com>
2025-07-30 11:24:43 -0500

    Update particles.js

--- particles.js
+++ particles.js
@@ -3,7 +3,7 @@ particlesJS('particles-js',
     {
         "particles": {
             "number": {
-                "value": 1000,
+                "value": 500,
                 "density": {
                     "enable": true,
                     "value_area": 450
@@ -97,7 +97,7 @@ particlesJS('particles-js',
                     "speed": 3
                 },
                 "repulse": {
-                    "distance": 200,
+                    "distance": 100,
                     "duration": 0.4
                 },
                 "push": {
