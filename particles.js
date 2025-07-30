4a6f6a6e1163943171d02e117a78fe21a8dbc1df
Tristan Parmerlee <parmerleetris@gmail.com>
2025-07-30 14:00:02 -0500

    Update particles.js

--- particles.js
+++ particles.js
@@ -56,7 +56,7 @@ particlesJS('particles-js',
             },
             "move": {
                 "enable": true,
-                "speed": 4.5,
+                "speed": 3.5,
                 "direction": "none",
                 "random": false,
                 "straight": false,
